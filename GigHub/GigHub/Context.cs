1539a392837ec7490238b1a15f12670461ab5c6e
Araujo, Carlos de <carlinhos10002@hotmail.com>
2017-03-24 18:01:46 -0500

    Create the Gig Model.

---
+++ GigHub/GigHub/Context.cs
@@ -0,0 +1,11 @@
+﻿using GigHub.Models;
+using System.Data.Entity;
+
+namespace GigHub
+{
+    public class Context : DbContext
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
