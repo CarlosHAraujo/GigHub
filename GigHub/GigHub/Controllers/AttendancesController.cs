9fb7371f8ee0823c1358e14c238f08a694d0c25c
Araujo, Carlos de <carlinhos10002@hotmail.com>
2017-03-29 11:44:11 -0500

    Add attendances.

---
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private Context _context;
+
+        public AttendancesController()
+        {
+            _context = new Context();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto gigId)
+        {
+            string userId = User.Identity.GetUserId();
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId.GigId))
+            {
+                return BadRequest("The attendance already exists.");
+            }
+            var attendance = new Attendance
+            {
+                GigId = gigId.GigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
